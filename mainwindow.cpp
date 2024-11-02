33b46a83255844e33cf10b995fcc52cf0da5978c
Mingyu Wu <wumingyu@email.com>
2024-11-02 14:38:31 +0800

    finish statusLine update design

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -18,11 +18,11 @@ MainWindow::MainWindow(QWidget *parent)
     textChanged = false;
     on_actionNew_triggered();
 
-    statusLabel.setMaximumWidth(150);
+    statusLabel.setMaximumWidth(180);
     statusLabel.setText("length: "+QString::number(0)+"       lines: "+QString::number(1));
     ui->statusbar->addPermanentWidget(&statusLabel);
 
-    statusCursorLabel.setMaximumWidth(150);
+    statusCursorLabel.setMaximumWidth(180);
     statusCursorLabel.setText("Ln: "+QString::number(0)+"       Col: "+QString::number(1));
     ui->statusbar->addPermanentWidget(&statusCursorLabel);
 
@@ -176,6 +176,9 @@ void MainWindow::on_textEdit_textChanged()
         this->setWindowTitle("*" + this->windowTitle());
         textChanged = true;
     }
+
+    statusLabel.setText("length: "+QString::number(ui->textEdit->toPlainText().length())+"       lines: "+
+                        QString::number(ui->textEdit->document()->lineCount()));
 }
 
 bool MainWindow::userEditConfirmed()
@@ -332,5 +335,27 @@ void MainWindow::on_actionExit_triggered()
 {
     if(userEditConfirmed())
         exit(0);
+}
+
+
+void MainWindow::on_textEdit_cursorPositionChanged()
+{
+    int col = 0;
+    int ln = 0;
+    int flg = -1;
+    int pos = ui->textEdit->textCursor().position();
+    QString text = ui->textEdit->toPlainText();
+
+    for(int i = 0;i < pos;i ++){
+        if(text[i] == '\n'){
+            ln++;
+            flg = i;
+        }
+    }
+
+    flg++;
+    col = pos - flg;
+
+    statusCursorLabel.setText("Ln: "+QString::number(ln+1)+"       Col: "+QString::number(col+1));
 }
 
--- mainwindow.h
+++ mainwindow.h
@@ -69,6 +69,8 @@ private slots:
 
     void on_actionExit_triggered();
 
+    void on_textEdit_cursorPositionChanged();
+
 private:
     Ui::MainWindow *ui;
 
